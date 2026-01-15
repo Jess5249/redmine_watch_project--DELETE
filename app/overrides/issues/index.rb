efe07fa0cbf364354307e0eb9b85ed4612c6ccb4
OWNER <183957814+Jess5249@users.noreply.github.com>
2026-01-14 23:16:45 -0600

    Delete app directory
    ABUSE - UNAUTHORIZED

--- app/overrides/issues/index.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new :virtual_path  => "issues/index",
-                     :name          => "add-watch-link-to-issues-list",
-                     :insert_top    => "div.contextual",
-                     :partial       => "redmine_watch_project/issues_watch_link"
